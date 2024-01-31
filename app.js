9e50de1dacf6b92c84a7cbf1686f47fd15a5eace
Ishaan Malik <141331814+ishaanmalik06@users.noreply.github.com>
2024-01-31 16:50:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,87 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector(".reset");
+let newBtn = document.querySelector(".new");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector(".msg");
+
+let turnO = true;
+let count = 0;
+
+let winChance = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 4, 6],
+  [2, 5, 8],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+const resetGame = () => {
+  let turnO = true;
+  count = 0;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO === true) {
+      turnO = false;
+      box.style.color = "#2A1E5C";
+      box.innerText = "O";
+    } else {
+      turnO = true;
+      box.style.color = "#841C26";
+      box.innerText = "X";
+    }
+    box.disabled = true;
+    count++;
+    let isWinner = checkWinner();
+    if (count === 9 && !isWinner) {
+      gameDraw();
+    }
+  });
+});
+
+const gameDraw = () => {
+  msg.innerText = "Game is a Draw";
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations, Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  for (let val of winChance) {
+    let pos1 = boxes[val[0]].innerText;
+    let pos2 = boxes[val[1]].innerText;
+    let pos3 = boxes[val[2]].innerText;
+
+    if (pos1 !== "" && pos2 !== "" && pos3 !== "") {
+      if (pos1 === pos2 && pos2 === pos3) {
+        showWinner(pos1);
+      }
+    }
+  }
+};
+
+resetBtn.addEventListener("click", resetGame);
